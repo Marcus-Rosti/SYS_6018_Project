317b12a50871f965edb7d28c2e1c7293ad3d0af7
nm1291 <nikhil.mascarenhas@gmail.com>
2015-11-28 18:46:25 -0500

    parallelized data read

--- data_parsing/createDataStructures.R
+++ data_parsing/createDataStructures.R
@@ -7,6 +7,7 @@ library(jsonlite)
 library(sqldf)
 library(rjson)
 library(plyr)
+library(parallel)
 
 business <- data.frame()
 checkin <- data.frame()
@@ -15,43 +16,90 @@ tip <- data.frame()
 user <- data.frame()
 
 business_json_file <- "../data/json/yelp_academic_dataset_business.json"
-checkin_json_file  <- "../data/json/yelp_academic_dataset_checkin.json"
+#checkin_json_file  <- "../data/json/yelp_academic_dataset_checkin.json"
 review_json_file <- "../data/json/yelp_academic_dataset_review.json"
-tip_json_file <- "../data/json/yelp_academic_dataset_tip.json"
+#tip_json_file <- "../data/json/yelp_academic_dataset_tip.json"
 user_json_file <- "../data/json/yelp_academic_dataset_user.json"
 
 
-convertJSONtoFlatStructure <- function(filepath){
-  json <- paste(readLines(filepath),collapse=",")
-  json <- fromJSON(paste("{\"dat\":[",json,"]}"))
-  json.i <- lapply(json$dat, function(x){ unlist(x)}) 
-  data <- (lapply((1:length(json.i)), 
-               function(x) {print(x);do.call("data.frame", as.list(json.i[[x]]))} ))
-  data2 <- data.frame()
-  for(i in 1:length(data)){
-    print(i)
-    data2 <- rbind.fill(data2,data[[i]])
+rbind.fill.parallel <- function(list, numCores){
+  while(length(list) > 1) {
+    idxlst <- seq(from=1, to=length(list), by=2)
+    
+    list <- mclapply(idxlst, function(i) {
+      print(i)
+      if(i==length(list)) { return(list[[i]]) }
+      return(rbind.fill(list[[i]], list[[i+1]]))
+    },mc.cores = getOption("mc.cores", numCores))
+    gc()
   }
-  return(data2)
+  return(list)
 }
 
 createFlatFiles <- function(){
   
-  business <- convertJSONtoFlatStructure(business_json_file)
-  save(business,file="../data/rdata/flatfile raw dump/business_raw_flatfile.R")
-  
-  #checkin <- convertJSONtoFlatStructure(checkin_json_file)
-  #save(checkin,file="../data/rdata/flatfile raw dump/checkin_raw_flatfile.R")
-  
-  review <- convertJSONtoFlatStructure(review_json_file)
-  save(review,file="../data/rdata/flatfile raw dump/review_raw_flatfile.R")
-  
-  
-  #tip <- convertJSONtoFlatStructure(tip_json_file)
-  #save(tip,file="../data/rdata/flatfile raw dump/tip_raw_flatfile.R")
+  #businesses
+  data <- paste(readLines(business_json_file),collapse=",")
+  data <- fromJSON(paste("{\"dat\":[",data,"]}"))
+  data <- lapply(data$dat, function(x){ unlist(x)}) 
+  data <- (mclapply((1:length(data)), 
+                    function(x) {print(x);do.call("data.frame", as.list(data[[x]]))}, mc.cores = getOption("mc.cores", 2L) ))
+  gc()
+  business <- rbind.fill.parallel(data,2L)[[1]]
+  save(business,file="../data/rdata/business_raw_flatfile.RData")
+  gc()
   
   
-  user <- convertJSONtoFlatStructure(user_json_file)
-  save(user,file="../data/rdata/flatfile raw dump/user_raw_flatfile.R")
+  #reviews
+  data <- paste(readLines(review_json_file),collapse=",")
+  data <- fromJSON(paste("{\"dat\":[",data,"]}"))
+  data <- lapply(data$dat, function(x){ unlist(x)}) 
+  gc()
+  reviews <- vector()
+  i <- 0
+  while((100000*i+1)<length(data)){
+    if(((i+1)*100000)<length(data)){
+      data1 <- (mclapply(((100000*i+1):((i+1)*100000)), 
+                        function(x) {print(x);do.call("data.frame", as.list(data[[x]]))}, mc.cores = getOption("mc.cores", 2L) ))
+      
+    }else{
+      data1 <- (mclapply(((100000*i+1):length(data)), 
+                function(x) {print(x);do.call("data.frame", as.list(data[[x]]))}, mc.cores = getOption("mc.cores", 2L) ))
+     
+    }
+    reviews[i+1] <- rbind.fill.parallel(data1)
+    save(reviews,file="../data/rdata/revuews_part.RData")
+    gc()
+    i <- i+1
+  }
+  
+  #users
+  data <- paste(readLines(user_json_file),collapse=",")
+  data <- fromJSON(paste("{\"dat\":[",data,"]}"))
+  data <- lapply(data$dat, function(x){ unlist(x)}) 
+  data <- (mclapply((1:length(data)), 
+                    function(x) {
+                        print(x)
+                        df <- do.call("data.frame", as.list(data[[x]]))
+                        df <- df[,names(df)[!(grepl(x=names(df),pattern = ".*friends.*"))]]  
+                        return(df)
+                      }, mc.cores = getOption("mc.cores", 2L) ))
+  gc()
+  users1 <- rbind.fill.parallel(data[1:100000],2L)[[1]]
+  save(users1,file="../data/rdata/users1_raw_flatfile.RData")
+  gc()
+  users2 <- rbind.fill.parallel(data[100001:200000],2L)[[1]]
+  save(users2,file="../data/rdata/users2_raw_flatfile.RData")
+  gc()
+  users3 <- rbind.fill.parallel(data[200001:300000],2L)
+  save(users3,file="../data/rdata/users3_raw_flatfile.RData")
+  gc()
+  users4 <- rbind.fill.parallel(data[300000:length(data)],2L)[[1]]
+  save(users4,file="../data/rdata/users4_raw_flatfile.RData")
+  gc()
+  
+  users <- rbind.fill(users1,users2,users3,users4)
+  save(users,file="../data/rdata/users_raw_flatfile.RData")
+  gc()
 
 }
