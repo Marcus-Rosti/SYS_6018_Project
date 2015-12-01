7fd856d5495ca3898da0e4ffc1ab8c1fa3061132
nm1291 <nikhil.mascarenhas@gmail.com>
2015-12-01 11:17:44 -0500

    Added code to process tips and checkins, just in case

--- data_parsing/createDataStructures.R
+++ data_parsing/createDataStructures.R
@@ -16,9 +16,9 @@ tip <- data.frame()
 user <- data.frame()
 
 business_json_file <- "../data/json/yelp_academic_dataset_business.json"
-#checkin_json_file  <- "../data/json/yelp_academic_dataset_checkin.json"
+checkin_json_file  <- "../data/json/yelp_academic_dataset_checkin.json"
 review_json_file <- "../data/json/yelp_academic_dataset_reviews.json"
-#tip_json_file <- "../data/json/yelp_academic_dataset_tip.json"
+tip_json_file <- "../data/json/yelp_academic_dataset_tip.json"
 user_json_file <- "../data/json/yelp_academic_dataset_user.json"
 
 
@@ -107,4 +107,41 @@ createFlatFiles <- function(){
   save(users,file="../data/rdata/users_raw_flatfile.RData")
   gc()
 
+  #check-in
+  data <- paste(readLines(checkin_json_file),collapse=",")
+  data <- fromJSON(paste("{\"dat\":[",data,"]}"))
+  data <- lapply(data$dat, function(x){ unlist(x)}) 
+  data <- (mclapply((1:length(data)), 
+                    function(x) {print(x);do.call("data.frame", as.list(data[[x]]))}, mc.cores = getOption("mc.cores", 2L) ))
+  gc()
+  checkin <- rbind.fill.parallel(data,2L)[[1]]
+  save(checkin,file="../data/rdata/checkin_raw_flatfile.RData")
+  gc()
+  
+  #tip
+  parts <- c('a','b','c','d','e')
+  for(part in parts){
+    print(paste("Part =",part))
+    file <- paste0(tip_json_file,"_part_a",part)
+    data <- paste(readLines(file),collapse=",")
+    data <- fromJSON(paste("{\"dat\":[",data,"]}"))
+    data <- lapply(data$dat, function(x){ unlist(x)}) 
+    data <- (mclapply((1:length(data)), 
+                      function(x) {print(x);do.call("data.frame", as.list(data[[x]]))}, mc.cores = getOption("mc.cores", 2L) ))
+    gc()
+    tips <- rbind.fill.parallel(data,2L)[[1]]
+    save(tips,file=paste0("../data/rdata/tips_raw_flatfile_part_",part,".RData"))
+    rm(data,file,tips)
+  }
+  
+  x <- parts[1]
+  load(file=paste0("../data/rdata/tips_raw_flatfile_part_",x,".RData"))
+  tip_data <-  tips
+  for(i in 2:length(parts)){
+    x <- parts[i]
+    load(file = paste0("../data/rdata/tips_raw_flatfile_part_",x,".RData"))
+    tip_data <- rbind.fill(tip_data,tips)
+  }
+  tips <- tip_data
+  save(tips,file="../data/rdata/tips_raw_flatfile.RData")
 }
