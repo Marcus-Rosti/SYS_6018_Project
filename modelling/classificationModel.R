947263833eb919712435c9e9617e81090f3dcbaf
nm1291 <nikhil.mascarenhas@gmail.com>
2015-12-08 18:54:15 -0500

    LocationMetrics

--- modelling/classificationModel.R
+++ modelling/classificationModel.R
@@ -5,12 +5,27 @@ load("../data/rdata/reviews_clean.RData")
 users <- users_clean
 
 users$topicDist <- rep(NA,nrow(users_clean))
-users$locationMetric <- rep(NA,nrow(users_clean))
+users$locationMetricHalfMile <- rep(NA,nrow(users_clean))
+users$locationMetricOneMile <- rep(NA,nrow(users_clean))
 users$predictedStars <- rep(NA,nrow(users_clean))
 users$visited <- rep(0,nrow(users_clean))
 
+#drop unrequired columns
 drop <- c("yelping_since","name","type")
 users <- users[,setdiff(names(users),drop)]
+
+#remove NAs from compliments columns
+users$compliments.profile <- as.numeric(ifelse(is.na(users$compliments.profile),0,users$compliments.profile))
+users$compliments.cute <- as.numeric(ifelse(is.na(users$compliments.cute),0,users$compliments.cute))
+users$compliments.funny <- as.numeric(ifelse(is.na(users$compliments.funny),0,users$compliments.funny))
+users$compliments.plain <- as.numeric(ifelse(is.na(users$compliments.plain),0,users$compliments.plain))
+users$compliments.writer <- as.numeric(ifelse(is.na(users$compliments.writer),0,users$compliments.writer))
+users$compliments.note <- as.numeric(ifelse(is.na(users$compliments.note),0,users$compliments.note))
+users$compliments.photos <- as.numeric(ifelse(is.na(users$compliments.photos),0,users$compliments.photos))
+users$compliments.hot <- as.numeric(ifelse(is.na(users$compliments.hot),0,users$compliments.hot))
+users$compliments.cool <- as.numeric(ifelse(is.na(users$compliments.cool),0,users$compliments.cool))
+users$compliments.more <- as.numeric(ifelse(is.na(users$compliments.more),0,users$compliments.more))
+users$compliments.list <- as.numeric(ifelse(is.na(users$compliments.list),0,users$compliments.list))
 
 ## restaurant of interest: "Flavors of Louisiana" : mwiNm868yAo8Xh8hO7Ke_Q
 ## populate visited
@@ -27,11 +42,29 @@ restTopicDiff <- topicDiff[,"mwiNm868yAo8Xh8hO7Ke_Q"]
 users$topicDist <- ifelse(is.na(restTopicDiff[users$user_id]),1,restTopicDiff[users$user_id])
 
 
-## populate locationMetric
+## populate locationMetricHalfMile
+load("../data/rdata/locationMetricHalfMile.RData")
+users$locationMetricHalfMile <- locationMetricHalfMile[users$user_id]
 
+## populate locationMetricOneMile
+load("../data/rdata/locationMetricOneMile.RData")
+users$locationMetricOneMile <- locationMetricOneMile[users$user_id]
 
 ## populate predictedStars
 
+
+# change variable type
+users$votes.funny  <- as.numeric(users$votes.funny)
+users$votes.useful  <- as.numeric(users$votes.useful)
+users$votes.cool <- as.numeric(users$votes.cool)
+users$review_count <- as.numeric(users$review_count)
+users$fans <- as.numeric(users$fans)
+users$average_stars <- as.numeric(users$average_stars)
+users$topicDist <- as.numeric(users$topicDist)
+users$locationMetricHalfMile <- as.numeric(users$locationMetricHalfMile)
+users$locationMetricOneMile <- as.numeric(users$locationMetricOneMile)
+users$predictedStars <- as.numeric(users$predictedStars)
+users$visited <- as.numeric(users$visited)
 
 ###
 # Split into train-test
@@ -42,4 +75,4 @@ train.data <- users[indices,]
 test.data <-  users[-indices,]
 
 ###
-# Logistic Model / or / SVM
+# Logistic Model \ or \ SVM
--- modelling/locationMetric.R
+++ modelling/locationMetric.R
@@ -1,38 +1,90 @@
 load("../data/rdata/business_clean.RData")
 load("../data/rdata/users_clean.RData")
 load("../data/rdata/reviews_clean.RData")
-library(sqldf)
 
-dist.prop <- function(points.1, thresh = 100){
-  dist.prop <- vector(mode = "numeric", length = nrow(points.1))
-  for (i in 1:nrow(points.1)){
-    lon.1 <- points.1[i,1]
-    lat.1 <- points.1[i,2]
-    dist <- vector(mode = "numeric", length = nrow(points.1))
-    for (j in 1:nrow(points.1)){
-      lon.2 <- points.1[j,1]
-      lat.2 <- points.1[j,2]
-      dist[j] <- gdist(lon.1, lat.1, lon.2, lat.2, units = "miles", a = 6378137.0, b = 6356752.3142, verbose = FALSE)
-    }
-    dist.prop[i] <- length(which(dist <= thresh))/nrow(points.1)
-  }
-  return(dist.prop)
-}
+require(sqldf)
+require(Imap)
+require(parallel)
 
 users <- users_clean$user_id
 rests <- business_clean$business_id
 
-locationMetric <- matrix(,nrow=length(users),ncol=1)
-dimnames(topicDiff)[[1]] <- users
-dimnames(topicDiff)[[2]] <- "mwiNm868yAo8Xh8hO7Ke_Q"
-
-radius <- 1
-
+radius <- 0.5
 active_restaurant <- "mwiNm868yAo8Xh8hO7Ke_Q"
-for(i in 1:nrow())
-dist.prop()
 
+lon.1 <- as.numeric(as.character(business_clean$longitude[business_clean$business_id=="mwiNm868yAo8Xh8hO7Ke_Q"]))
+lat.1 <- as.numeric(as.character(business_clean$latitude[business_clean$business_id=="mwiNm868yAo8Xh8hO7Ke_Q"]))
 
-for(i in users){
-    
+near_by_restaurants <- c()   
+for(rest in rests){
+  lon.2 <- as.numeric(as.character(business_clean$longitude[business_clean$business_id==rest]))
+  lat.2 <- as.numeric(as.character(business_clean$latitude[business_clean$business_id==rest]))
+  dist <- gdist(lon.1, lat.1, lon.2, lat.2, units = "miles", a = 6378137.0, b = 6356752.3142, verbose = FALSE)
+  if(dist<radius)
+    near_by_restaurants <- c(near_by_restaurants,rest)
 }
+near_by_restaurants <- setdiff(near_by_restaurants,c("mwiNm868yAo8Xh8hO7Ke_Q"))
+near_by_restaurants_sql <- paste(paste0("'",near_by_restaurants,"'"),collapse=",")
+reviews_near_by <- sqldf(paste0("SELECT user_id FROM reviews_clean WHERE business_id IN (",near_by_restaurants_sql,")"))
+users_near_by_count <- sqldf("SELECT user_id, count(*) as count FROM reviews_near_by GROUP BY user_id")
+
+locationMetric <- matrix(,nrow=length(users),ncol=1)
+dimnames(locationMetric)[[1]] <- users
+dimnames(locationMetric)[[2]] <- "mwiNm868yAo8Xh8hO7Ke_Q"
+
+calcLocationMetric <- function(i){
+  print(i)
+  u <- users[i]
+  if(u %in% users_near_by_count$user_id){
+    n <- users_near_by_count$count[users_near_by_count$user_id==u]
+  }
+  else{
+    n <- 0
+  }
+  locationMetric[u,1] <- n/length(near_by_restaurants)
+}
+locMet <- sapply(1:length(users),calcLocationMetric)
+locationMetric[,1] <- locMet
+locationMetricHalfMile <- locationMetric[,1] 
+save(locationMetricHalfMile,file="../data/rdata/locationMetricHalfMile.RData")
+
+####################
+radius <- 1
+active_restaurant <- "mwiNm868yAo8Xh8hO7Ke_Q"
+
+lon.1 <- as.numeric(as.character(business_clean$longitude[business_clean$business_id=="mwiNm868yAo8Xh8hO7Ke_Q"]))
+lat.1 <- as.numeric(as.character(business_clean$latitude[business_clean$business_id=="mwiNm868yAo8Xh8hO7Ke_Q"]))
+
+near_by_restaurants <- c()   
+for(rest in rests){
+  lon.2 <- as.numeric(as.character(business_clean$longitude[business_clean$business_id==rest]))
+  lat.2 <- as.numeric(as.character(business_clean$latitude[business_clean$business_id==rest]))
+  dist <- gdist(lon.1, lat.1, lon.2, lat.2, units = "miles", a = 6378137.0, b = 6356752.3142, verbose = FALSE)
+  if(dist<radius)
+    near_by_restaurants <- c(near_by_restaurants,rest)
+}
+near_by_restaurants <- setdiff(near_by_restaurants,c("mwiNm868yAo8Xh8hO7Ke_Q"))
+near_by_restaurants_sql <- paste(paste0("'",near_by_restaurants,"'"),collapse=",")
+reviews_near_by <- sqldf(paste0("SELECT user_id FROM reviews_clean WHERE business_id IN (",near_by_restaurants_sql,")"))
+users_near_by_count <- sqldf("SELECT user_id, count(*) as count FROM reviews_near_by GROUP BY user_id")
+
+locationMetric <- matrix(,nrow=length(users),ncol=1)
+dimnames(locationMetric)[[1]] <- users
+dimnames(locationMetric)[[2]] <- "mwiNm868yAo8Xh8hO7Ke_Q"
+
+calcLocationMetric <- function(i){
+  print(i)
+  u <- users[i]
+  if(u %in% users_near_by_count$user_id){
+    n <- users_near_by_count$count[users_near_by_count$user_id==u]
+  }
+  else{
+    n <- 0
+  }
+  locationMetric[u,1] <- n/length(near_by_restaurants)
+}
+locMet <- sapply(1:length(users),calcLocationMetric)
+locationMetric[,1] <- locMet
+locationMetricOneMile <- locationMetric[,1] 
+save(locationMetricOneMile,file="../data/rdata/locationMetricOneMile.RData")
+
